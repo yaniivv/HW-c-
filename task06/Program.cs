3bbd703a223ec13338ce07acc3231e4a40f5bcc2
yaniivv <yanaa.unicorn@gmail.com>
2022-10-03 19:20:39 +0500

    Решение третьей задачи

---
+++ task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if ((N % 2) == 0) 
+{
+    Console.WriteLine("Да");
+}
+else 
+{
+    Console.WriteLine("Нет");
+}
