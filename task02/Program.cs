9c11b589e8ac7f9aae08af3d955416fa3f5853b6
yaniivv <yanaa.unicorn@gmail.com>
2022-10-03 13:42:25 +0500

    sdelana diagramma

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
