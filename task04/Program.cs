fe2f4a4264cba56fd68add3a9bb2bd979c8459aa
yaniivv <yanaa.unicorn@gmail.com>
2022-10-03 18:22:50 +0500

    Решение второй задачи

---
+++ task04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int D = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (D > max) max = D;
+
+Console.Write("max = ");
+Console.WriteLine(max);
