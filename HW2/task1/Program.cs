98dfc1ae38b07efc942c7a28f377f3252aed016d
yaniivv <yanaa.unicorn@gmail.com>
2022-10-07 13:02:55 +0500

    hw2

---
+++ HW2/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int getnum(string text)
+{
+    Console.Write(text + ": ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
