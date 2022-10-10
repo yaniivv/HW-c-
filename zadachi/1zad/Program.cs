c4a323e41e22ab7f7decd8f6aba7517ac1fb8a18
yaniivv <yanaa.unicorn@gmail.com>
2022-10-10 20:05:25 +0500

    zadachki

---
+++ zadachi/1zad/Program.cs
@@ -0,0 +1,26 @@
+﻿
+// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void Num(int num)
+{
+
+    if (N < 100)
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    while (N < 1000)
+{
+    N = N % 10;
+    Console.WriteLine(N);
+    break;
+}
+}
+return;
+}
+
+Num(N);
