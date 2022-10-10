8a2815cf48e7d2ebab1aaaa1bd8f6323766284bc
yaniivv <yanaa.unicorn@gmail.com>
2022-10-10 21:32:06 +0500

    2 hw

--- HW2/task1/Program.cs
+++ HW2/task1/Program.cs
@@ -1,9 +1,28 @@
 ﻿// Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
 
-int getnum(string text)
+ void Num(double result)
 {
-    Console.Write(text + ": ");
-    int num = Convert.ToInt32(Console.ReadLine());
-    return num;
-}
+  
+  if(num<100)
+  {
+    Console.WriteLine("Error");
+  }
+  else
+  {
+    num = num % 100;
+    Console.WriteLine(num);
+  }
+  else
+  {
+    while(num < 1000)
+    {
+      num = num % 100;
+      Console.WriteLine(num);
+     break;
+    }
+  }
+
+Num(num);
 
