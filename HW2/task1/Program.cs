c23a816d916b7d259c7060db7ed72206164eb2d1
yaniivv <yanaa.unicorn@gmail.com>
2022-10-11 21:06:55 +0500

    2 HwwwWw

--- HW2/task1/Program.cs
+++ HW2/task1/Program.cs
@@ -1,25 +1,19 @@
 ﻿Console.WriteLine("Введите трёхзначное число:");
 int des = Convert.ToInt32(Console.ReadLine());
-string sec = Convert.ToString(des);
 
- void Num(int result)
- {
+int Num(int result)
+{
  if(des<100)
   {
     Console.WriteLine("Error");
   }
   else
   {
-    Console.WriteLine("Вторая цифра этого числа: "+sec[1]);
-  }
-  else
-  {
-    while(des < 1000)
+    while(des < 1000) des /= 10; 
     {
-     Console.WriteLine("-> "+sec[1]);
-      break;
+     Console.WriteLine(des % 10);
     }
   }
-
+}
 Num();
 
