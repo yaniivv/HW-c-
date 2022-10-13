0d989dd2a358869dc1f1fe4b72447619aadcfdfc
yaniivv <yanaa.unicorn@gmail.com>
2022-10-13 13:48:00 +0500

    4 HWww

---
+++ HW4/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//опишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B
+
+Console.WriteLine("Введите число A:");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+
+for(int i = 1; i <= numB; i++)
+{
+   res*= numA;
+}
+  Console.WriteLine($"{res} = {numA} ^ {numB}");
+
+
