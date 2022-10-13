65117e22b9f33b181528384975bab57e0bd6cff6
yaniivv <yanaa.unicorn@gmail.com>
2022-10-13 15:35:23 +0500

    4 HwWww

---
+++ HW4/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumR(int num)
+{
+    int ind = Convert.ToString(num).Length;
+    int result = 0;
+    int prom = 0;
+
+    for (int i = 0; i < ind; i++){
+      prom = num - num % 10;
+      result = result + (num - prom);
+      num = num / 10;
+    }
+   return result;
+}
+
+int sum = SumR(num);
+Console.WriteLine($"{num} -> {sum}");
