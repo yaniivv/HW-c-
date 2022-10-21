2546efb1ecf4675371abfbce71c4558750660292
yaniivv <yanaa.unicorn@gmail.com>
2022-10-21 22:19:38 +0500

    5 HWww

---
+++ HW5/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] numbers = new int[10];
+
+void GetArray(int[] array, int min, int max){
+for (int i = 0; i<array.Length; i++ ){
+array[i] = new Random().Next(min, max);
+}
+}
+void WriteArray(int[] array){
+for (int i = 0; i<array.Length; i++ ){
+Console.Write(array[i] + " ");
+}
+}
+
+int QuantityPositive(int[] array){
+int quantity = 0;
+for (int i = 0; i<array.Length; i++ ){
+if (array[i] % 2 == 1)
+{
+quantity++;
+}
+}
+return quantity;
+}
+
+GetArray(numbers, 100, 1000);
+WriteArray(numbers);
+
+int quantity = QuantityPositive(numbers);
+Console.Write($"-> {quantity}");
