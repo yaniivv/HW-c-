14b3cf4a589dc36d9551e5dba43f16c67183f068
yaniivv <yanaa.unicorn@gmail.com>
2022-10-28 18:53:35 +0500

    5 Hwwwwww

---
+++ HW5/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] arrayNew = new double[10];
+  for (int i = 0; i < arrayNew.Length; i++ )
+  {
+    arrayNew[i] = new Random().Next(1, 100);
+    Console.Write(arrayNew[i] + " ");
+  }
+
+double maxNumber = arrayNew[0];
+double minNumber = arrayNew[0];
+
+  for (int i = 1; i < arrayNew.Length; i++)
+  {
+    if (maxNumber < arrayNew[i])
+    {
+      maxNumber = arrayNew[i];
+    }
+        if (minNumber > arrayNew[i])
+    {
+      minNumber = arrayNew[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.Write($"\n-> {decision}");
+
+
+
