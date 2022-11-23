ef0a9852c1d8ba705ad94644a203666159e873cb
yaniivv <yanaa.unicorn@gmail.com>
2022-11-23 14:37:21 +0500

    3 hw

---
+++ proekt/3 task/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine($"\nВведите размер массива X x Y x Z:");
+int x = InputNumbers("Введите X: ");
+int y = InputNumbers("Введите Y: ");
+int z = InputNumbers("Введите Z: ");
+Console.WriteLine($"");
+
