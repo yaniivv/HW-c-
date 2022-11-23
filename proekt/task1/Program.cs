15b7cf02826d36d0d3ea348d05c5ead9be546569
yaniivv <yanaa.unicorn@gmail.com>
2022-11-23 13:48:32 +0500

    1 hw

---
+++ proekt/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿int InputNumber(string input)
+{
+     Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+ }
+
+ void PrintSum(int m, int n, int sum)
+ {
+     sum = sum + n;
+     if (n <= m)
+    {
+         Console.Write($" {sum} ");
+         return;
+     }
+     PrintSum(m, n - 1, sum);
+
+ }
+ int m = InputNumber("Ввод m: ");
+ int n = InputNumber("Ввод n: ");
+ int temp = m;
+
+ if (m > n)
+ {
+     m = n;
+     n = temp;
+ }
+ Console.Write($"{m}, {n} ->");
+ PrintSum(m, n, temp = 0);
