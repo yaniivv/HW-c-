6a08e4d06f57f77118575ce1f64af8cea0f91845
yaniivv <yanaa.unicorn@gmail.com>
2022-11-23 13:54:44 +0500

    2 hw

---
+++ proekt/2 task/Program.cs
@@ -0,0 +1,28 @@
+﻿int InputNumber(string input)
+{
+Console.Write(input);
+int output = Convert.ToInt32(Console.ReadLine());
+return output;
+}
+int Akkerman(int m, int n)
+{
+if (m == 0)
+{
+return n + 1;
+}
+else if (n == 0 && m > 0)
+{
+return Akkerman(m - 1, 1);
+}
+else
+{
+return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+}
+
+int m = InputNumber("Ввод m: ");
+int n = InputNumber("Ввод n: ");
+int akkermanFunction = Akkerman(m, n);
+Console.Write($"m = {m}, n = {n} - > A(m,n) = {akkermanFunction} ");
+    
+
